1b170df33a57a263d3a4aa09e10837c87dc303ec
Scott Finneran <scottfinneran@yahoo.com.au>
2012-10-12 09:18:10 +1100

    Small tidy of README and added copyright notice to source file

--- windsock.py
+++ windsock.py
@@ -1,4 +1,5 @@
-# This file is part of Windsock.
+# Windsock - wifi modem status and management
+# Copyright (C) 2012 Scott Finneran
 #
 # Windsock is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
